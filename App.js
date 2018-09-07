e4358b5a190afe9484403ac0e2e497c16c010350
Matt41531 <dxfan218@yahoo.com>
2018-09-07 17:50:44 -0400

    Added comments

--- App.js
+++ App.js
@@ -3,8 +3,7 @@ import HomeScreen from './HomeTab';
 import SecondScreen from './SecondTab';
 import ThirdScreen from './ThirdScreen';
 
-
-//This file will serve only for navigation
+//This file will serve only for navigation, import the files for pages above then list them below
 export default createBottomTabNavigator({
   Home: HomeScreen,
   Second: SecondScreen,
