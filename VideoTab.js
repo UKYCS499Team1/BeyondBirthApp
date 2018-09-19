09b282a1a832eeb986dea1d4027bb5231c461b99
Matt41531 <dxfan218@yahoo.com>
2018-09-19 13:34:26 -0400

    Added control bar for the video without buttons

--- VideoTab.js
+++ VideoTab.js
@@ -8,15 +8,41 @@ export default class VideoScreen extends Component {
       return (
         <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
           <Text>Anything could go here!</Text>
-
+          <View>
           <Video
           source={{ uri: 'http://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4' }}
           shouldPlay
           resizeMode="cover"
           style={{ width, height: 300 }}
-/>
+/>  
+
+          <View style={styles.controlBar}></View>
+          </View>
+
+          
 
         </View>
       );
     }
   }
+
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#ffffff',
+      alignItems: 'center',
+      justifyContent: 'center'
+    },
+
+    controlBar: {
+      position: 'absolute',
+      bottom: 0,
+      left: 0,
+      right: 0,
+      height: 25,
+      flexDirection: 'row',
+      alignItems: 'center',
+      justifyContent: 'center',
+      backgroundColor: 'rgba(52, 52, 52, 0.9)'
+    }
+  })
