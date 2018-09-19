84b48e48a0d95b4afebba0c21abba20eef3d1edd
Matt41531 <dxfan218@yahoo.com>
2018-09-19 13:53:39 -0400

    Added play and mute buttons to video tab

--- VideoTab.js
+++ VideoTab.js
@@ -1,8 +1,29 @@
 import React, {Component}  from 'react';
 import { Text, View, Dimensions, StyleSheet } from 'react-native';
 import { Video } from 'expo';
+import { MaterialIcons, Octicons } from '@expo/vector-icons';
 
 export default class VideoScreen extends Component {
+    state = {
+      mute: false,
+      shouldPlay: true
+    }
+
+    handlePlay = () => {
+      this.setState((prevState) => ({
+        shouldPlay: !prevState.shouldPlay
+      })
+      )
+    }
+
+    handleMute = () => {
+      this.setState((prevState) => ({
+        mute: !prevState.mute
+      })
+      )
+    }
+
+
     render() {
       const { width } = Dimensions.get('window');
       return (
@@ -11,12 +32,31 @@ export default class VideoScreen extends Component {
           <View>
           <Video
           source={{ uri: 'http://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4' }}
-          shouldPlay
+          shouldPlay= {this.state.shouldPlay}
+          isMuted={this.state.mute}
           resizeMode="cover"
           style={{ width, height: 300 }}
 />  
 
-          <View style={styles.controlBar}></View>
+          <View style={styles.controlBar}>
+
+          <MaterialIcons 
+          name={this.state.mute ? "volume-mute" : "volume-up"}
+          size={45} 
+          color="white" 
+          onPress={this.handleMute} 
+          />
+
+          <MaterialIcons 
+          name={this.state.shouldPlay ? "pause" : "play-arrow"} 
+          size={45}
+          color="white" 
+          onPress={this.handlePlay} 
+          />
+
+          </View>
+
+
           </View>
 
           
@@ -39,7 +79,7 @@ export default class VideoScreen extends Component {
       bottom: 0,
       left: 0,
       right: 0,
-      height: 25,
+      height: 45,
       flexDirection: 'row',
       alignItems: 'center',
       justifyContent: 'center',
