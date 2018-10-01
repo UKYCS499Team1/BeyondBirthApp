0fa67a3af29dccd6c0be4cfa317caea71c1c9c2d
codejst <jste223@g.uky.edu>
2018-10-01 16:11:12 -0400

    I added the moods with individual radio buttons

--- ThirdScreen.js
+++ ThirdScreen.js
@@ -1,12 +1,32 @@
-import React, {Component}  from 'react';
+//import RadioForm from 'react-native-simple-radio-button';
+import React, { Component } from 'react';
 import { Text, View } from 'react-native';
+import RadioForm, {
+    RadioButton,
+    RadioButtonInput,
+    RadioButtonLabel
+} from 'react-native-simple-radio-button';
 
-export default class ThirdScreen extends Component {
+var moods = [
+    { label: "Happy", value: 0 },
+    { label: "Average", value: 1 },
+    { label: "Sad", value: 2 },
+    { label: "Depressed", value: 3 },
+    { label: "Stressed", value: 4 },
+];
+
+export default class ThirdScreen extends Component
+{
     render() {
       return (
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-          <Text>Third Screen</Text>
-        </View>
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>             
+              <Text>Welcome to your moodlet manager</Text>
+              <RadioForm
+                  radio_props={moods}
+                  intial={2}                  
+                  onPress={(value) => {}}
+              />              
+          </View>         
       );
     }
   }
