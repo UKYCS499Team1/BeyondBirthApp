3b7495e946d7917f3f372572d18f7c66cae97a1e
Matt41531 <dxfan218@yahoo.com>
2018-11-01 14:37:01 -0400

    Added state-changing functionality to buttons on Medicine Reminders page and restyled page

--- MedicineRemindersScreen.js
+++ MedicineRemindersScreen.js
@@ -2,35 +2,84 @@ import React, {Component}  from 'react';
 import { Text, View, TouchableOpacity, StyleSheet } from 'react-native';
 
 export default class LocateScreen extends Component {
+  constructor(props) {
+    super(props);
+  
+    this.state = {
+      remindersStatus: false
+    }
+  }
+
+  handleStatusOn = () => {
+    this.setState(() => ({
+      remindersStatus: true
+    })
+    )
+    console.log(this.state.remindersStatus)
+  }
+  handleStatusOff = () => {
+    this.setState(() => ({
+      remindersStatus: false
+    })
+    )
+    console.log(this.state.remindersStatus)
+  }
     render() {
       return (
         <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
 
-        <Text> Medicine Reminders </Text>
+        <Text style={styles.text}> Medicine Reminders </Text>
+        <View style={styles.container} >
           <TouchableOpacity
          style={styles.button}
+         onPress={this.handleStatusOn} 
          
        >
-       <Text > ON </Text>
+       <Text style={styles.buttonText}> ON </Text>
         </TouchableOpacity>
         <TouchableOpacity
          style={styles.button}
+         onPress={this.handleStatusOff} 
          
        >
-       <Text > OFF </Text>
+       <Text style={styles.buttonText} > OFF </Text>
         </TouchableOpacity>
-
-        <Text> Sends a daily reminder to take medicines </Text>
+        </View>
+        <Text style={styles.smallText}> Sends a daily reminder to take medicines </Text>
         </View>
       );
     }
   }
 
   const styles= StyleSheet.create({
+      container: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+  
+
+      },
+
+      text: {
+        fontSize: 25,
+        fontWeight: 'bold'
+      },
+
+      smallText: {
+        fontSize: 15
+      },
+
+      buttonText: {
+        fontWeight: 'bold'
+      },
+
       button: {
-        flexDirection: 'column',
-        height: 20,
-        justifyContent: 'center',
-        backgroundColor: '#f4aa89',
+        flexDirection: 'row',
+        height: 30,
+        justifyContent: 'space-between',
+        padding: 25,
+        alignItems: 'center',
+        backgroundColor: '#8f7291',
       }
+
   })
